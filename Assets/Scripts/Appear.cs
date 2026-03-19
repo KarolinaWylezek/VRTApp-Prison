06a7b40af0ab07498d52fc32fa73d802b713265e
ITFCWI-MTPOJ8N2\karolina <karolina.v.wylezek@gmail.com>
2026-03-19 12:07:23 +0100

    Added script that make an object appear after given time

---
+++ Assets/Scripts/Appear.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Appear : NetworkCollider
+{
+    public GameObject button;
+    public GameObject sound;
+    public GameObject nun;
+
+
+
+    // Start is called before the first frame update
+    public override void Trigger()
+    {
+        StartCoroutine(EnableAfterTime());
+    }
+
+    IEnumerator EnableAfterTime()
+    {
+        yield return new WaitForSeconds(20.0f);
+        button.SetActive(true);
+        sound.SetActive(true);
+        nun.SetActive(true);
+
+
+    }
+}
+
